8a08b235089f9d456128f7e1d86a5e3c0f05e37e
Pramesh Pudasaini <prameshpudasaini@gmail.com>
2023-03-09 17:05:38 -0700

    Processed incident data

--- script/geocode_incident_location.R
+++ script/geocode_incident_location.R
@@ -35,3 +35,37 @@ register_google(key = api_key)
 result <- geocode(incident_data_NA$Address, output = "latlona", source = "google")
 
 # saveRDS(result, "data/crash_geocode_result.rds")
+
+result <- readRDS("data/crash_geocode_result.rds")
+
+incident_data_geocoded <- bind_cols(incident_data_NA, result)
+
+incident_data_final <- incident_data %>% 
+    filter(is.na(Longitude) | is.na(Latitude)) %>% 
+    left_join(incident_data_geocoded %>% 
+                  select(IncidentID, lon, lat), by = "IncidentID") %>% 
+    mutate(Longitude = lon,
+           Latitude = lat) %>% 
+    select(-lon, -lat) %>% 
+    bind_rows(incident_data %>% 
+                  filter(!is.na(Longitude))) %>% 
+    filter(!is.na(Longitude)) %>% 
+    select(IncidentID, InjurySeverity, RouteName, Latitude, Longitude)
+
+incident_data_sf <- st_as_sf(incident_data_final,
+                             coords = c("Longitude", "Latitude"),
+                             crs = "+proj=longlat +datum=WGS84")
+
+# saveRDS(incident_data_sf, "data/incident_data_sf.rds")
+
+# leaflet map incidents
+pal <- colorFactor(palette = "RdYlGn", domain = incident_data_final$InjurySeverity)
+
+incident_data_final %>% 
+    leaflet() %>% 
+    addTiles() %>% 
+    addProviderTiles(providers$CartoDB.Positron, group = "CartoDB") %>% 
+    addCircleMarkers(lng = ~Longitude, lat = ~Latitude, color = pal(incident_data_final$InjurySeverity),
+                     radius = ~InjurySeverity) %>% 
+    addLegend("topright", pal = pal, values = incident_data_final$InjurySeverity, 
+              opacity = 3, title = "Injury Severity")
