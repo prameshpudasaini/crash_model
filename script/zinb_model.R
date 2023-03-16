d60ec356b08519a9b2f6a16915c1b79761ec90b2
Pramesh Pudasaini <prameshpudasaini@gmail.com>
2023-03-15 23:29:19 -0700

    Created ZINB model2
    Difference between percentile speed models

--- script/zinb_model.R
+++ script/zinb_model.R
@@ -35,3 +35,33 @@ stargazer(m_pos_kabco, m_pos_ka, m_pos_bc, m_pos_o,
           star.cutoffs = c(.05, .01, .001),
           title = "Model 1: Operating Speed Percentiles",
           no.space = TRUE, digits = 3)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Model 2: Difference within Percentile Speed Models
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+dpos_kabco <- mdata %>% 
+    dplyr::select(KABCO, AADT, PSL, Length, Road_Class, P50_P15, P85_P50, P85_P15)
+dpos_ka <- mdata %>% 
+    dplyr::select(K_A, AADT, PSL, Length, Road_Class, P50_P15, P85_P50, P85_P15)
+dpos_bc <- mdata %>% 
+    dplyr::select(B_C, AADT, PSL, Length, Road_Class, P50_P15, P85_P50, P85_P15)
+dpos_o <- mdata %>% 
+    dplyr::select(O, AADT, PSL, Length, Road_Class, P50_P15, P85_P50, P85_P15)
+
+m_dpos_kabco <- glm.nb(KABCO ~ log(AADT) + PSL + Length + Road_Class + P50_P15 + P85_P50,
+                       data = dpos_kabco)
+m_dpos_ka <- glm.nb(K_A ~ log(AADT) + PSL + Length + Road_Class + P50_P15 + P85_P50,
+                    data = dpos_ka)
+m_dpos_bc <- glm.nb(B_C ~ log(AADT) + PSL + Length + Road_Class + P50_P15 + P85_P50,
+                    data = dpos_bc)
+m_dpos_o <- glm.nb(O ~ log(AADT) + PSL + Length + Road_Class + P50_P15 + P85_P50,
+                   data = dpos_o)
+
+stargazer(m_dpos_kabco, m_dpos_ka, m_dpos_bc, m_dpos_o, 
+          type = "html", 
+          out = "output/m2_diff_operating_speed_percentiles.html",
+          star.cutoffs = c(.05, .01, .001),
+          title = "Model 2: Difference within Operating Speed Percentiles",
+          no.space = TRUE, digits = 3)
